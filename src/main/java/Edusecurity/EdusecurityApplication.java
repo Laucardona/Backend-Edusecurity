1444c345979f01f817b34817b05fd569bd79f0dc
Laucardona <lauris.042006@gmail.com>
2026-06-18 13:17:04 -0500

    feat:Estructura backend proyecto

---
+++ src/main/java/Edusecurity/EdusecurityApplication.java
@@ -0,0 +1,13 @@
+package Edusecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdusecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdusecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Edusecurity/EdusecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package Edusecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdusecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
